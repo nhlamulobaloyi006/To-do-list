a1abe694910bfc652be306fabab1a5470b5853b4
Nhlamulo <nhlamulob7@gmail.com>
2026-08-22 22:14:08 +0200

    Implement user input validation and enhance UI layout for welcome and dashboard sections

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
-import { greet } from "./script";
+import { welcomeMain, dashboardMain, activityMain, usernameMsg, emailMsg, username, email, submitBtn, taskCount, remainingCount, completedCount, totalCount, dashboardInput } from "./script.js";
+import { validateUsername, validateEmail, disableByDefault} from "./script.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.addEventListener("click", function() {
-        alert("")
-    })
-})
+let storeCredentials = []
+
+disableByDefault()
+
+function getUserCredentials() {
+    const load = localStorage.getItem("userlogin");
+
+    if (load) {
+        const convert = JSON.parse(localStorage.getItem("userlogin"));
+        console.log(convert);
+        storeCredentials = [...convert]
+        console.log(storeCredentials)
+        welcomeMain.style.display = "none";
+        dashboardMain.style.display = "flex";
+    } else {
+        welcomeMain.style.display = "flex";
+    }
+}
+
+getUserCredentials();
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    username.addEventListener("input", () => {
+        validateUsername()
+    });
+    email.addEventListener("input", () => {
+        validateEmail()
+    });
+    submitBtn.addEventListener("click", () =>{
+        alert("Account was created successfully");
+        welcomeMain.style.display = "none";
+        dashboardMain.style.display = "flex";
+        const savedCredentials = {
+            username: username.value,
+            email: email.value
+        }
+        storeCredentials.push(savedCredentials)
+        console.log(storeCredentials)
+        localStorage.setItem("userlogin", JSON.stringify(storeCredentials));
+        
+    });
+});
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    dashboardInput.addEventListener("click", ()=>{
+        activityMain.style.display = "flex"; 
+        dashboardMain.style.display = "none";
+        welcomeMain.style.display = "none"; 
+    });
+});
+
+
+
+function waning() {
+    alert("hy")
+}
+
--- script.js
+++ script.js
@@ -1,5 +1,78 @@
-export const greet =  function warning() {
-    alert("Hello world");
+//Parent-divs
+export const welcomeMain = document.querySelector(".welcome-main");
+export const dashboardMain = document.querySelector(".dashboard-main");
+export const activityMain = document.querySelector(".activity-main");
+
+//error messages
+export const usernameMsg = document.getElementById("username-msg");
+export const emailMsg = document.getElementById("email-msg");
+
+//welcome form
+export const username = document.getElementById("username");
+export const email = document.getElementById("email");
+export const submitBtn = document.getElementById("submitBtn");
+
+//count  Email is not valid
+export const taskCount = document.getElementById("taskCount");
+export const remainingCount = document.getElementById("remainingCount");
+export const completedCount = document.getElementById("completedCount");
+export const totalCount = document.getElementById("completedCount");
+
+//
+export const dashboardInput = document.getElementById("dashboardInput");
+
+export function disableByDefault() {
+    submitBtn.disabled = true;
+    submitBtn.style.cursor = "not-allowed";
+}
+disableByDefault()
+
+export function validateUsername() {
+    if (username.value.trim().length === 0 || username.value.trim().length < 5) {
+        usernameMsg.style.color = "red";
+        usernameMsg.textContent = "Username is not valid";
+        submitBtn.style.cursor = "not-allowed";
+        submitBtn.disabled = true;    
+    }
+    else if (username.value.trim().length > 0 && email.value.trim().length < 5 || !email.value.trim().includes("@gmail.com")) {
+        usernameMsg.style.color = "green";
+        usernameMsg.textContent = "Username is valid";
+        emailMsg.style.color = "red";
+        emailMsg.textContent = "Email is not valid";
+        submitBtn.style.cursor = "not-allowed";
+        submitBtn.disabled = true;    
+    }
+    else if (username.value.trim().length > 0 && email.value.trim().length > 0 && email.value.trim().includes("@gmail.com")) {
+        usernameMsg.style.color = "green";
+        usernameMsg.textContent = "Username is valid";
+        submitBtn.style.cursor = "pointer";
+        submitBtn.disabled = false;    
+    }
 }
 
-warning();
+export function validateEmail() {
+    if (email.value.trim().length === 0 || !email.value.trim().includes("@gmail.com")) {
+        emailMsg.style.color = "red";
+        emailMsg.textContent = "Email is not valid";
+        submitBtn.style.cursor = "not-allowed";
+        submitBtn.disabled = true;
+        
+    } else if (email.value.trim().length > 0 && username.value.trim().length < 5 && email.value.trim().includes("@gmail.com")) {
+        emailMsg.style.color = "green";
+        emailMsg.textContent = "Email is valid";
+        usernameMsg.textContent = "Username is not valid"
+        submitBtn.style.cursor = "not-allowed";
+        submitBtn.disabled = true;
+    }
+    else if (email.value.trim().length > 0 && username.value.trim().length >= 5  && email.value.trim().includes("@gmail.com")) {
+        emailMsg.style.color = "green";
+        emailMsg.textContent = "Email is valid";
+        submitBtn.style.cursor = "pointer";
+        submitBtn.disabled = false;
+    }
+}
+
+
+
+
+
